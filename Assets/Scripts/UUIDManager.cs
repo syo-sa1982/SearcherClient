2cc73075828ec7d54ae91583b6639e390ef1844e
syo-sa1982 <fujii1308@gmail.com>
2015-09-02 08:05:07 +0900

    first commit.

---
+++ Assets/Scripts/UUIDManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UUIDManager : MonoBehaviour 
+{
+	private string _uuid;
+
+	void Awake()
+	{
+		System.Guid guid = System.Guid.NewGuid();
+		_uuid = guid.ToString();
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void Save()
+	{
+		if (PlayerPrefs.HasKey ("uuid")) {
+			Debug.Log ("持ってる");
+			return;
+		}
+
+		PlayerPrefs.SetString("uuid",_uuid);
+		PlayerPrefs.Save();
+	}
+
+	public void Load()
+	{
+		_uuid = PlayerPrefs.GetString ("uuid");
+		Debug.Log (_uuid);
+	}
+}
