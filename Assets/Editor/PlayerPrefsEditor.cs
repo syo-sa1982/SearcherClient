3043c4338621eea20a062e7da2f602d62326941b
syo-sa1982 <fujii1308@gmail.com>
2015-09-04 08:00:37 +0900

    取得したUUIDをサーバーに投げるところまで出来た。それとエディタ拡張

---
+++ Assets/Editor/PlayerPrefsEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class PlayerPrefsEditor
+{
+	[MenuItem("Tools/PlayerPrefs/DeleteAll")]
+	static void DeleteAll()
+	{
+		PlayerPrefs.DeleteAll();
+		Debug.Log("Delete All Data Of PlayerPrefs!!");
+	}
+}
--- Assets/Scripts/UUIDManager.cs
+++ Assets/Scripts/UUIDManager.cs
@@ -11,6 +11,9 @@ public class UUIDManager : MonoBehaviour
 			System.Guid guid = System.Guid.NewGuid();
 			_uuid = guid.ToString();
 		}
+		Debug.Log ("awake");
+		Debug.Log (_uuid);
+
 	}
 
 	// Use this for initialization
@@ -25,20 +28,36 @@ public class UUIDManager : MonoBehaviour
 	
 	}
 
-	public IEnumerator AddNewUser()
-	{
-	}
-
 	public void Save()
 	{
-		if (PlayerPrefs.HasKey ("uuid")) {
-			Debug.Log ("持ってる");
-			return;
-		}
+		Debug.Log ("Save");
+		Debug.Log (_uuid);
 
 		PlayerPrefs.SetString("uuid",_uuid);
 		PlayerPrefs.Save();
+
+		StartCoroutine(AddNewUser());
+	
 	}
+
+	public IEnumerator AddNewUser()
+	{
+		Debug.Log ("AddNewUser");
+		Debug.Log (_uuid);
+
+		string url = "http://localhost:8000/user/add";
+		WWWForm wwwForm = new WWWForm ();
+
+		wwwForm.AddField ("uuid", _uuid);
+		wwwForm.AddField ("roll_count", "3");
+
+		WWW www = new WWW(url, wwwForm);
+
+		yield return www;
+	}
+
+
+
 
 	public void Load()
 	{
