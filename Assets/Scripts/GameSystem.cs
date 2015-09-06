009eff89da81566272ddcfce5563b8cd4d797e11
syo-sa1982 <fujii1308@gmail.com>
2015-09-06 22:44:26 +0900

    Scene追加 todo:認証の追加

---
+++ Assets/Scripts/GameSystem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSystem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void GameStart()
+	{
+		
+		StartCoroutine (CheckUser());
+	}
+
+	public IEnumerator CheckUser()
+	{
+		if (PlayerPrefs.HasKey ("uuid")) {
+			Debug.Log ("持ってる");
+			yield return null;
+		} else {
+			Debug.Log ("持って無い");
+			yield return null;
+			Application.LoadLevel ("Signup");
+		}
+	}
+}
