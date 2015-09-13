d030f6210e4cfefc172e5edb6ac51d222688cea0
syo-sa1982 <fujii1308@gmail.com>
2015-09-13 23:03:25 +0900

    何回ロールするかをサーバーに投げる

--- Assets/Scripts/CharaMake.cs
+++ Assets/Scripts/CharaMake.cs
@@ -6,6 +6,44 @@ using MiniJSON;
 
 public class CharaMake : MonoBehaviour {
 
+	[SerializeField]
+	private int Strength;
+	[SerializeField]
+	private int Constitution;
+	[SerializeField]
+	private int Power;
+	[SerializeField]
+	private int Dextality;
+	[SerializeField]
+	private int Appeal;
+	[SerializeField]
+	private int Size;
+	[SerializeField]
+	private int Intelligence;
+	[SerializeField]
+	private int Education;
+
+	private Dictionary<string, int[]> RollData = new Dictionary<string, int[]> ();
+
+	void Awake () 
+	{
+		int [] roll3D6 = new int[]{ 6, 3 };// 3D6
+		int [] roll2D6Plus6 = new int[]{ 6, 2, 6 };// 2D6+6
+		int [] roll3D6Plus3 = new int[]{ 6, 3, 3 };// 3D6+3
+
+		RollData.Add ("Strength" , roll3D6);
+		RollData.Add ("Constitution", roll3D6);
+		RollData.Add ("Power", roll3D6);
+		RollData.Add ("Dextality", roll3D6);
+		RollData.Add ("Appeal", roll3D6);
+
+		RollData.Add ("Size", roll2D6Plus6);
+		RollData.Add ("Intelligence", roll2D6Plus6);
+
+		RollData.Add ("Education", roll3D6Plus3);
+
+	}
+
 	// Use this for initialization
 	void Start () {
 	
@@ -28,13 +66,14 @@ public class CharaMake : MonoBehaviour {
 	public IEnumerator GetDiceRollResult()
 	{
 		string url = "http://localhost:8000/player/base_make";
-		//		WWWForm form = new WWWForm ();
-		//
-		//		form.AddField ("uuid", _uuid);
-		//
-		//		Debug.Log ("持ってる");
+		WWWForm form = new WWWForm ();
 
-		WWW www = new WWW(url);
+		foreach (KeyValuePair<string,int[]> data in RollData) {
+			form.AddField (data.Key, data.Value);
+		}
+
+
+		WWW www = new WWW(url, form);
 
 		yield return www;
 
