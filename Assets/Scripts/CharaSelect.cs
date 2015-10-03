f8f1df2aa71e4288759cc3f259ec746d04e25ab7
syo-sa1982 <fujii1308@gmail.com>
2015-10-03 22:34:29 +0900

    キャラ選択を作る

--- Assets/Scripts/CharaSelect.cs
+++ Assets/Scripts/CharaSelect.cs
@@ -4,12 +4,27 @@ using System.Collections;
 public class CharaSelect : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		StartCoroutine (GetCharaList ());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public IEnumerator GetCharaList()
+	{
+		Debug.Log ("GetCharaList");
+
+		string url = ConfURL.URL_DEBUG+ConfURL.USER_ADD;
+		WWWForm form = new WWWForm ();
+
+		form.AddField ("roll_count", "3");
+
+		WWW www = new WWW(url, form);
+		yield return www;
+
 	}
 }
