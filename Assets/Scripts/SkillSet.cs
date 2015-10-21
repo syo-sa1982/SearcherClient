01febb3bb7adc5d020ba57467a05f6aeaa42b5f8
syo-sa1982 <fujii1308@gmail.com>
2015-10-21 09:28:51 +0900

    受け取ったデータを表示

--- Assets/Scripts/SkillSet.cs
+++ Assets/Scripts/SkillSet.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -50,14 +51,21 @@ public class SkillSet : MonoBehaviour
 			var playerStatus = skillSetAPI ["PlayerStatus"] as Dictionary<string,object>;
 			foreach(var data in skillList) {
 				var SkillData = data.Value as Dictionary<string,object>;
-				Debug.Log(data.Key);
-				Debug.Log (SkillData ["ID"]);
-				foreach(var dataValue in SkillData) {
-					Debug.Log(dataValue.Key);
-					Debug.Log(dataValue.Value);
-				}
+//				Debug.Log ((int)SkillData ["Value"]);
+//				foreach(var dataValue in SkillData) {
+//					Debug.Log(dataValue.Key);
+//					Debug.Log(dataValue.Value);
+//				}
 				GameObject skillField = (GameObject)Instantiate(Resources.Load("Prefabs/SkillSet/SkillField"));
 				skillField.transform.SetParent(canvasObject.transform,false);
+
+				GameObject skillName = skillField.transform.FindChild ("SkillName").gameObject;
+				GameObject skillValue = skillField.transform.FindChild ("SkillValue").gameObject;
+				var nameText = skillName.GetComponent<Text>();
+				var valueText = skillValue.gameObject.GetComponent<InputField>();
+				nameText.text = (string)SkillData ["SkillName"];
+				int SkillDataValue = System.Convert.ToInt32(SkillData ["Value"]);
+				valueText.text = SkillDataValue.ToString();
 			}
 		}
 	}
