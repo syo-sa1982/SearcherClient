2ae4558c42276f9bcc8b07606e89d48378e43e9f
syo-sa1982 <fujii1308@gmail.com>
2016-01-02 20:01:06 +0900

    MAPの外枠

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MapGenerator : MonoBehaviour {
+public class MapGenerator : MonoBehaviour 
+{
+
+	public GameObject defaultWallPrefab;
+	private int default_x_max = 19;
+	private int default_z_max = 19;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		CreateMapOuterframe();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void CreateMapOuterframe()
+	{
+		for(int dx = 0; dx <= default_x_max; dx++){
+			Instantiate(defaultWallPrefab, new Vector3(dx, 0, 0), Quaternion.identity);
+			Instantiate(defaultWallPrefab, new Vector3(dx, 0, default_z_max), Quaternion.identity);
+		}
+		
+		for(int dz = 0; dz <= default_z_max; dz++){
+			Instantiate(defaultWallPrefab, new Vector3(0, 0, dz), Quaternion.identity);
+			Instantiate(defaultWallPrefab, new Vector3(default_x_max, 0, dz), Quaternion.identity);
+		}
+		
 	}
 }
