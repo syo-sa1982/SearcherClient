d59b62e2ac292435941dafe9f5e7eb3ea6ae7e8f
syo-sa1982 <fujii1308@gmail.com>
2016-01-03 20:38:09 +0900

    追尾カメラ

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour 
+{
+	public GameObject player;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 1.25f, player.transform.position.z - 1);
+	}
+}
