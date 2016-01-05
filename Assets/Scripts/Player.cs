6ad30f25cb0776658f36be1b924acd6b277ed2be
syo-sa1982 <fujii1308@gmail.com>
2016-01-06 08:59:27 +0900

    とりあえず動かした

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,7 +17,12 @@ public class Player : MonoBehaviour
 		targetDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 		
 		if (targetDirection.magnitude > 0.1) {
-			GetComponent<Animation>().Play("Take 001");
+			//プレイヤーの向きを変えて
+			transform.rotation = Quaternion.LookRotation(targetDirection);
+			//CharacterControllerコンポーネントを呼び出し
+			CharacterController conroller = GetComponent<CharacterController>();
+			//移動
+			conroller.Move(transform.forward * Time.deltaTime * 3f);
 		}
 	}
 }
